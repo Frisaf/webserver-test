241c475600842d33cb67266397af11d5ec506b7e
Frisaf <frisaf07@gmail.com>
2025-08-27 11:01:07 +0200

    more server stuff

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ import express from "express";
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello world!")
+    res.send("Hello TE23 välkomna :D")
 });
 
+app.get("/about", (req, res) => {
+    res.json({
+        "message": "Blubb"
+    })
+})
+
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on http://localhost:3000")
 })
