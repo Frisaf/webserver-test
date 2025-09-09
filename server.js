4b3dc9255dccf1c2c0ccd3c8ca5afdc97c43acd6
Frisaf <frisaf07@gmail.com>
2025-09-09 09:40:40 +0200

    queery params and morgan

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import nunjucks from "nunjucks"
+import morgan from "morgan"
 
 const app = express()
 
+app.use(morgan("dev"))
 app.use(express.static("public"))
 
 nunjucks.configure("views", {
@@ -22,6 +24,11 @@ app.get("/about", (req, res) => {
         title: "About",
         message: "Detta är ett skolarbete"
     })
+})
+
+app.get("/greeting", (req, res) => {
+    res.send(`HALLOJ I STUGAN ${req.query.name}! ${req.query.message}!`)
+    console.log(req.query.hey)
 })
 
 app.listen(3000, () => {
